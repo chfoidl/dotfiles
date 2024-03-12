1ff5d624e923b1fbac0bf15c6361881f7ab7ff02
Christian Foidl <christian@wunderwerk.io>
2024-03-12 09:54:43 +0100

    feat: properly setup git in home-manager to use secrets

--- nixos/users/chfoidl/home/modules/git.nix
+++ nixos/users/chfoidl/home/modules/git.nix
@@ -1,11 +1,14 @@
 { ... }:
 {
   sops.secrets."git-config" = {
-    path = "/home/chfoidl/.config/git/config";
+    path = "/home/chfoidl/.config/git/custom-config";
   };
 
   programs.git = {
     enable = true;
+    includes = [
+      { path = "~/.config/git/custom-config"; }
+    ];
   };
 }
 
